f85cf4412468c5714a0dba873473e668fd8efcde
Jamesparker14 <36118293+Jamesparker14@users.noreply.github.com>
2018-02-10 07:14:17 -0500

    Project Start
    Adding base Maven files so that eclipse can pick up the project

---
+++ src/main/java/cop2805/App.java
@@ -0,0 +1,13 @@
+package cop2805;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
