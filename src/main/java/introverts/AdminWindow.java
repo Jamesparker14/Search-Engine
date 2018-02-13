475f6897fb5a4697a03669b2cd6291e0ab72ad6c
Kritsia Figueroa <kfigueroa13@hawkmail.hccfl.edu>
2018-02-13 01:13:08 -0500

    This is what I have so far. Not finished yet but will keep updating.

--- src/main/java/introverts/AdminWindow.java
+++ src/main/java/introverts/AdminWindow.java
@@ -5,29 +5,63 @@
 // Project created by team introverts:
 // James Parker, Jessica Parker, Kritsia Figueroa
 
+
+
 package introverts;
-
-
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import javax.swing.*;
 
 
 public class AdminWindow extends Frame
 	{
+	
+	private JFrame f;
+	private JPanel p;
+	private JLabel title;
+	private JButton addFiles;
+	private JButton removeFiles;
+	private JButton updateFiles;
+	
 	   public AdminWindow ()
 	   {
-	      setTitle("Admin Mode");
-	      setSize(800, 600);
-	      setVisible(true);
+		   
+		  f = new JFrame("Admin Mode");
+		  f.setVisible(true);
+	      f.setSize(800, 600);
+	      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      
+	      title = new JLabel("Search Engine - Administrator", JLabel.CENTER);
+	      title.setFont(new Font("Tahoma", Font.BOLD, 30));
+		  title.setAlignmentX(0);
+		  title.setAlignmentY(0);
+		  f.add(title,BorderLayout.NORTH);
 
-	      addWindowListener(
-	         new WindowAdapter()
-	         {  public void windowClosing(WindowEvent e)
-	            {  System.exit(0);
-	            }
-	         }
-	      );
+	      JPanel p = new JPanel(new GridBagLayout());
+	      
+	      JButton addFiles = new JButton("Add Files");
+	      JButton removeFiles = new JButton("Remove Files");
+	      JButton updateFiles = new JButton("Update Files");
+	      
+	      GridBagConstraints c = new GridBagConstraints();
+	      
+	      c.insets = new Insets(60,60,60,60);
+	      c.gridx = 1;
+	      c.gridy = 0;
+	      p.add(addFiles, c);
+	      
+	      c.gridx = 2;
+	      c.gridy = 0;
+	      p.add(removeFiles, c);
+	      
+	      c.gridx = 3;
+	      c.gridy = 0;
+	      p.add(updateFiles, c);
+	
+	      f.add(p,BorderLayout.SOUTH);
 	   }
-
-}
+	}
